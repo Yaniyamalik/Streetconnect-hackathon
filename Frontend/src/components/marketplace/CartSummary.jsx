17d412b3c0d0164879fb9b3a77eff9254c35eceb
Yaniya <140041434+Yaniyamalik@users.noreply.github.com>
2025-07-27 19:44:30 +0530

    Update CartSummary.jsx

--- Frontend/src/components/marketplace/CartSummary.jsx
+++ Frontend/src/components/marketplace/CartSummary.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useCart } from "../../context/CartContext";
+import { useCart } from "../../context/Cartcontext";
 
 const CartSummary = () => {
   const { cart } = useCart();
