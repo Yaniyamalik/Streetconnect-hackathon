8dfe0a3f5fd248702e6a606643baabd73fee8e8e
Yaniya <140041434+Yaniyamalik@users.noreply.github.com>
2025-07-27 20:34:04 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,10 +3,14 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 const app=express()
 
-app.use(cors({
- origin:process.env.ORIGIN_NAME,
- credentials: true
-}))
+
+app.use(
+  cors({
+    origin: "https://streetconnect-hackathon-llxj.vercel.app/", // ← your Vercel frontend domain
+    credentials: true,
+  })
+);
+
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
