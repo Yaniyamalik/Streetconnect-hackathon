a8969dd421ec7f909d1dfd87c78bd76791929b8c
Yaniya <140041434+Yaniyamalik@users.noreply.github.com>
2025-07-27 20:23:05 +0530

    Update authService.js

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -2,25 +2,25 @@
 import axios from 'axios';
 
 export const signup = async (data) => {
-  const response = await axios.post('https://streetconnect-hackathon.vercel.app/api/users/signup', data, {
+  const response = await axios.post('https://streetconnect-hackathon.onrender.com/api/users/signup', data, {
     withCredentials: true, // for cookie/token
   });
   return response.data;
 };
 
 export const login=async (data)=>{
-  const response = await axios.post('https://streetconnect-hackathon.vercel.app/api/users/login', data, {
+  const response = await axios.post('https://streetconnect-hackathon.onrender.com/api/users/login', data, {
     withCredentials: true, // for cookie/token
   });
   return response.data;
 }
 export const logout = async () => {
-  const response = await axios.post('https://streetconnect-hackathon.vercel.app/api/users/logout', {}, {
+  const response = await axios.post('https://streetconnect-hackathon.onrender.com/api/users/logout', {}, {
     withCredentials: true, // for cookie/token
   });
   return response.data;
 }
 export const getProfile = async () => {
-  const res = await axios.get('https://streetconnect-hackathon.vercel.app/api/v1/user/profile');
+  const res = await axios.get('https://streetconnect-hackathon.onrender.com/api/v1/user/profile');
   return res.data.data; // { user: { name, email, ... } }
 };
