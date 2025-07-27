d8a8b800c98205f0d606c8211ac6c592abd26db9
Yaniya <140041434+Yaniyamalik@users.noreply.github.com>
2025-07-27 19:17:11 +0530

    Update warehouseService.js

--- Frontend/src/services/warehouseService.js
+++ Frontend/src/services/warehouseService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL =  'http://localhost:5000/api';
+const API_URL =  'https://streetconnect-hackathon.vercel.app/api';
 
 /**
  * Fetches warehouses based on search and filter criteria
